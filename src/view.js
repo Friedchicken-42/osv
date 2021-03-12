298249b01e21adf14d654e5c12afd6af48b5a0a2
Friedchicken <paololego20+github@gmail.com>
2021-03-12 20:20:16 +0100

    ~from_beatmaps

--- src/view.js
+++ src/view.js
@@ -28,9 +28,8 @@ class View extends React.Component {
                 beatmap.difficultyrating = parseFloat(beatmap.difficultyrating).toFixed(2);
                 const promises = users.map(user => axios.get('https://osu.ppy.sh/api/get_scores', { params: { k: key, b: beatmap.beatmap_id, m: beatmap.mode, u: user, type: 'string' } }))
                 axios.all(promises).then(responses => {
-                    let scores = responses.map(r => r.data[0])
-
-                    console.log(beatmap.title)
+                    let scores = []
+                    responses.forEach(r => r.data.forEach(s => scores.push(s)))
                     this.setState({ decks: [...this.state.decks, <Deck beatmap={beatmap} scores={scores} rank={true} />] })
                 })
             })
